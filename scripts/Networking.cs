89895f3d33a73ea945aca5f0b7e6051ae26a5946
Aleksandr Savchenkov <asavchenkov1@gmail.com>
2020-07-31 16:31:18 -0700

    changed back to WebRTCPeerConnection

--- scripts/Networking.cs
+++ scripts/Networking.cs
@@ -28,6 +28,7 @@ public class Networking : Node
 	Godot.Collections.Dictionary RTCInitializer = new Godot.Collections.Dictionary();
 	
 	public WebRTCMultiplayer RTCMP = new WebRTCMultiplayer();
+
 	
 	HandshakeServer handshakeServer;
 	HandshakeClient handshakeClient;
@@ -54,21 +55,6 @@ public class Networking : Node
 	//maps the uid of the peer to the uid of the relay peer.
 	private Dictionary<int,Relay> peerRelays = new Dictionary<int,Relay>();
 
-	//for buffering Ice candidates when we have yet to set both sdp.
-	public class IceBuffer
-	{
-		int uid;
-		WebRTCPeerConnection peer;
-		private struct Candidate
-		{
-			string media;
-			int index;
-			string name;
-		}
-
-		public ArrayList candidates = new ArrayList();
-	}
-	Dictionary<int, IceBuffer> IceBuffers = new Dictionary<int, IceBuffer>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
